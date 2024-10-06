6bbaef2824567e70b23afc5d63d6cbd4501723ed
Sujal Raval <88503112+s123u@users.noreply.github.com>
2024-10-06 22:03:09 +0530

    Second Commit

--- app/src/main/java/com/example/alarmmanager/MainActivity.java
+++ app/src/main/java/com/example/alarmmanager/MainActivity.java
@@ -1,14 +1,24 @@
 package com.example.alarmmanager;
 
+import android.app.AlarmManager;
+import android.app.PendingIntent;
+import android.content.BroadcastReceiver;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
 
 import androidx.activity.EdgeToEdge;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContract;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+static final int ALARM_REQ_CODE = 100;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +30,33 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        EditText editText = findViewById(R.id.edt);
+
+        AlarmManager alarmManager = (AlarmManager) getSystemService(ALARM_SERVICE);
+
+        findViewById(R.id.btn).setOnClickListener(new View.OnClickListener()  {
+            @Override
+            public void onClick(View view) {
+                int time = Integer.parseInt(((EditText)(findViewById(R.id.edt))).getText().toString());
+                long trigger = System.currentTimeMillis()+(time*1000);
+
+                Intent intent = new Intent(MainActivity.this, MyReceiver.class);
+
+                PendingIntent pi = PendingIntent.getBroadcast(
+                        MainActivity.this,
+                        ALARM_REQ_CODE,
+                        intent,
+                        PendingIntent.FLAG_UPDATE_CURRENT | PendingIntent.FLAG_IMMUTABLE
+                );
+                alarmManager.set(alarmManager.RTC_WAKEUP,trigger,pi);
+            }
+        });
+
+
+
+
     }
+
+
 }
